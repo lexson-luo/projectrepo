2e72d97c744b13a0d0e7c329d3be77ae6f703e47
lexson-luo <lexson.luo@gmail.com>
2025-01-18 12:49:03 +0800

    update formatted string's float format to 2 decimals

--- feature.py
+++ feature.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     print("Please key in the second number: ")
     b = float(input())
     c = sum_total(a, b)
-    print("The sum of those two numbers is {.f}.".format(c))
+    print("The sum of those two numbers is {:.2f}.".format(c))
 
